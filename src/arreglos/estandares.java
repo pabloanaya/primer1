38b7a40e8a70c2fdda3c40aa4daa1f11d1355a7f
pabloanaya <pablo.anaya.flores09@gmail.com>
2025-08-22 11:38:18 -0600

    u

--- src/arreglos/estandares.java
+++ src/arreglos/estandares.java
@@ -1,10 +1,39 @@
 package arreglos;
 
+import java.util.Scanner;
+
 public class estandares {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		long[] telefonos;
+
+		Scanner captura = new Scanner(System.in);
+		// long[] telefonos2 = new telefonos2[5];
+		System.out.println("cuantos numeros quieres almacenar");
+		int sizeArray = captura.nextInt();
+		telefonos = new long[sizeArray];
+		long num_tel;
+		
+		for(int x=0; x<sizeArray;x++) {
+			
+			System.out.println("dame el telefono");
+			num_tel = captura.nextLong();
+			telefonos[x] = num_tel;
+		}
+	
+		int i;
+		for(int i1 = 0; i1 < telefonos.length ;i1++ ) {
+			System.out.println("el numero de tel es " +telefonos[i1]);
+			
+		}
 	}
+	
+	
+	
+	
+	
+	
 
 }
